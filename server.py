d9b997f91654f1e9e5684bdb5ea2b232f7d3a3a6
mattdyer <madmatt1220@gmail.com>
2017-03-13 13:47:14 -0600

    added code for basic web server

---
+++ server.py
@@ -0,0 +1,25 @@
+import wsgiref.simple_server
+import mimetypes
+
+def application(environ, start_response):
+	
+	status = '200 OK'
+	requestedPath = environ['PATH_INFO']
+	
+	content = ''
+	content_type = 'text/html'
+	
+	try:
+		file_content = open(requestedPath[1:])
+		content = file_content.read()
+		content_type = mimetypes.guess_type(requestedPath[1:])[0]
+	except IOError:
+		status = '404 Not Found'
+	
+	start_response(status,[('content-type', content_type + ';charset=utf-8')])
+	
+	
+	return [content.encode('utf-8')]
+
+server = wsgiref.simple_server.make_server('', 8000, application)
+server.serve_forever()
